87b1e2a7a9081a8c8cb59e2dbf90c76d18e22990
alex101 <hexuanhua101@gmail.com>
2020-04-04 14:09:20 +0800

    transport dictionary

--- src/client/client.py
+++ src/client/client.py
@@ -1,28 +1,100 @@
 import socket
-import struct
 import os
-if __name__ == '__main__':
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #建立TCP连接
+''' 增加选择传输文件OR文件夹的功能 '''
+''' 传输文件用标志0，传输文件夹用标志1，附带上文件夹中各文件大小'''
 
-    s.connect(('127.0.0.1', 9999))
-    #读取文件,生成文件头部信息
-    filepath = r'testcase.txt'
-    fp = open(filepath,'rb')
-    base_name = os.path.basename(filepath)
-    file_size = os.stat(filepath).st_size
-    fhead = base_name+','+str(file_size)
-    fhead = fhead.encode('utf-8')
+totalSize = 0 #传输目录的时候所有文件大小之和
 
-    message_info_size = str(len(fhead))#头部信息的长度，设定为100
-    while len(message_info_size)<100:
-        message_info_size = message_info_size+" "
-    s.send(message_info_size.encode('utf-8'))
+def transportFile(sock,filepath):
 
-    s.send(fhead)
+    fp = open(filepath, 'rb')
+    fhead = buildHeadInfo(filepath)
+    fhead_size = buildFheadSize(fhead)
+    sock.send(fhead_size.encode('utf-8')) #传输头部大小
+    sock.send(fhead.encode('utf-8')) #传输头部信息
     while True:
         data = fp.read(1024)
         if not data:
-            print('file send over')
             break
-        s.send(data)
+        sock.send(data)
+    print("already transport",filepath)
+
+def buildHeadInfo(filepath):
+    file_size = os.stat(filepath).st_size #发送的时候传绝对路径
+    fhead = filepath + "," + str(file_size)
+    return fhead
+def buildFheadSize(fhead):
+    message_info_size = str(len(fhead))
+    if len(message_info_size)<100:
+        message_info_size.ljust(100," ")
+    return message_info_size
+
+
+
+
+def transportDir(sock,totalFile):
+    for filepath in totalFile:
+        transportFile(sock,filepath)
+
+def sendControlBit(sock,func):
+    if func==1:
+        sock.send("0".encode('utf-8'))
+    if func==2:
+        sock.send("1".encode('utf-8'))
+
+def getAllFile(filepath,totalFile):
+    tmp = os.listdir(filepath)
+    global totalSize
+    for f in tmp:
+        f = os.path.join(filepath,f)
+        if os.path.isfile(f):
+            totalFile.append(f)
+            totalSize = totalSize+os.path.getsize(f)
+        else:
+            getAllFile(f, totalFile)
+
+
+
+
+if __name__ == '__main__':
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #建立TCP连接
+
+    totalFile = [] #储存需要发送的所有文件
+    s.connect(('127.0.0.1', 9999))
+    #读取文件,生成文件头部信息
+    while True:
+        print("choose function:")
+        print("input 1 for single file")
+        print("input 2 for files in dir")
+
+        function = eval(input())
+        print("input file name or dir name:")
+        filepath = input()
+        sendControlBit(s,function) #发送控制bit位
+
+        if function==1:
+            transportFile(s,filepath)
+            print("transport a file") #完成单个文件传输
+
+
+
+        if function==2:
+            getAllFile(filepath,totalFile)
+            base_path = os.path.split(filepath)[1]
+            if len(base_path)<100:
+                base_path.ljust(100," ")
+            s.send(base_path.encode('utf-8')) #发送根目录信息
+            totalSize = str(totalSize)
+            if len(totalSize)<100:
+                totalSize.ljust(100," ")
+            s.send(totalSize.encode('utf-8'))
+
+            transportDir(s,totalFile)
+
+
+
+        print("input exit to quit,other key to continue")
+        control = input()
+        if control=='exit':
+            break
     s.close()
--- src/server/server.py
+++ src/server/server.py
@@ -1,54 +1,71 @@
 import socket
-import struct
-import threading
+import os
+def dealFilePath(fileName,controlBit,baseName=""):
+    if controlBit==0:
+        return os.path.split(fileName)[1]
+    if controlBit==1:
+        index = fileName.find(baseName)
+        return fileName[index:]                #例如C:\User\Alex\FileTransport\text.py basename=FileTranport  可得到FileTransport\text.py
 
 
-def recv_data(sock,addr):
-    print("accept new connection from {0}".format(addr))
-    msglen = eval(sock.recv(100).decode('utf-8').strip(' '))#第一次先发送一个头部信息的长度
-    print(msglen)
-    buffer = sock.recv(msglen)
+def recv_data(sock,addr,controlBit,baseName=""):
+    msglen = eval(sock.recv(100).decode('utf-8').strip(' '))  # 第一次先发送一个头部信息的长度
+    buffer = sock.recv(msglen) #获取头部信息
     if buffer:
         file_head = (buffer.decode('utf-8')).split(',')
-        print("file_head")
-        print(file_head)
-        file_name = file_head[0]
+        filePath = dealFilePath(file_head[0],controlBit,baseName) #获取带路径的文件名，或是单纯的文件名
+        if controlBit==1:
+            base_path,Name = os.path.split(filePath)
+            if not os.path.exists(base_path):
+               os.makedirs(base_path)#创建层级目录
+        fileName = filePath #文件名，根据controlbit不同就带有不同的路径
+
         file_size = eval(file_head[1])
-        print('file new name is {0}, filesize is {1}'.format(file_name, file_size))
+        print('file new name is {0}, filesize is {1}'.format(fileName, file_size))
         recvd_size = 0
-        fp = open('./'+file_name,'wb')
+        fp = open(fileName, 'wb')
         print("begin receiving")
-        while recvd_size!=file_size:
-            if file_size-recvd_size>1024:
+        while recvd_size != file_size:
+            if file_size - recvd_size > 1024:
                 data = sock.recv(1024)
-                recvd_size+=len(data)
-                print("current size"+str(recvd_size))
+                recvd_size += len(data)
+                #print("current size" + str(recvd_size))
             else:
-                data= sock.recv(file_size-recvd_size)
+                data = sock.recv(file_size - recvd_size)
                 recvd_size = file_size
-                print("current size" + str(recvd_size))
+                #print("current size" + str(recvd_size))
             if not data:
                 break
             fp.write(data)
         fp.close()
     else:
         print("no file recv")
-
-    sock.close()
-    print("connection from %s:%s closed" % addr)
-
+    return  recvd_size
 
 
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)#防止不发送数据时tcp连接马上关闭，用于接受最后一部分数据
     s.bind(('0.0.0.0',9999))
-    s.listen(5)
+    s.listen(1)
     print('wait for connection......')
-    while True:
-        sock,addr = s.accept()
-        t = threading.Thread(target=recv_data,args=(sock,addr))
-        t.start()
-        break#测试时还没用到多线程，先break
+    sock,addr = s.accept() # 获得接收数据使用的套接字
+    print("accept new connection from {0}".format(addr))
+    controlBit= eval(sock.recv(1).decode('utf-8'))
+    if controlBit==0: #
+        recv_data(sock,addr,controlBit)
+        sock.close()
+        print("connection from %s:%s closed" % addr)
+    if controlBit==1:
+        base_Name = sock.recv(100).decode('utf-8').strip(" ")
+        total_size= 0
+        current_size = 0
+        total_size =eval(sock.recv(100).decode('utf-8').strip(" "))
+        while current_size!=total_size:
+            current_size+=recv_data(sock,addr,controlBit,base_Name)
+    sock.close()
+    s.close()
+
+
 
 
