ce216a887e91561bd679df195a261569095267e1
apple-chip <applechip@pm.me>
2020-04-08 12:35:51 +0800

    restore socket reuse

--- src/server/server.py
+++ src/server/server.py
@@ -43,6 +43,7 @@ if __name__ == '__main__':
     listen_port = int(sys.argv[3])
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((listen_addr, listen_port))
     s.listen(1)
     print('wait for connection......')
