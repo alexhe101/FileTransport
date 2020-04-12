f3655bd295bc9fb56bcd0ae250efb106f0a0ca22
apple-chip <applechip@pm.me>
2020-04-12 21:47:09 +0800

    read entire fire into memory

--- src/client/client.py
+++ src/client/client.py
@@ -11,54 +11,66 @@ def main():
     path = sys.argv[1]
     addr = sys.argv[2]
     port = int(sys.argv[3])
-    compress = (len(sys.argv) == 5)
+    compress = len(sys.argv) == 5
     # 发起连接
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((addr, port))
-    print(f"remote: {addr}:{port}, sending files from {path}")
+    print(f"connected to {addr}:{port}")
     # 加载所有文件及名称,发送文件
     for f in rread(path):
         send_file(sock, xfile(f[0], f[1], compress=compress))
     # 发送结束位
     sock.send(int(0).to_bytes(4, byteorder='big'))
     sock.close()
-    print("all sent")
+    print('connection closed')
+
+
+def aread(path):
+    data = bytearray()
+    with open(path, 'rb') as f:
+        buff = f.read(65535)
+        while buff:
+            data.append(buff)
+            buff = f.read(65535)
+    return bytes(data)
 
 
 def rread(path):
     path = Path(os.path.normpath((Path(path).absolute())))
-    return [[path.name, path.open("rb").read()]] if path.is_file()\
-        else [[item.relative_to(path.parent), item.open("rb").read()]
+    return [[path.name, aread(path)]] if path.is_file()\
+        else [[item.relative_to(path.parent), aread(item)]
               for item in path.rglob('*')
               if item.is_file()]
 
 
 class xfile():
     def __init__(self, path, data, compress=False):
-        self.name = str(path)  # 文件名：字节形式
-        self.md5 = hashlib.md5(data)  # 原文件特征
-        self.data = zlib.compress(data) if compress else data  # 数据（可能压缩）
-        self.name_size = len(self.name.encode('utf-8'))  # 文件名字节长度
-        self.data_size = len(self.data)  # 数据长度
-        self.compress = compress  # 压缩标志
+        self.name = str(path)
+        self.md5 = hashlib.md5(data)
+        self.data = zlib.compress(data) if compress else data
+        self.name_size = len(self.name.encode('utf-8'))
+        self.data_size = len(self.data)
+        self.compress = compress
 
 
 def send_file(sock, f):
-    # 发送文件名长度、文件名、摘要
+    # 发送文件名长度、文件名、摘要和压缩标志
     sock.send(f.name_size.to_bytes(4, byteorder='big'))
     sock.send(f.name.encode('utf-8'))
     sock.send(f.md5.digest())
+    sock.send((1 if f.compress else 0).to_bytes(1, byteorder='big'))
     print(f"file: {f.name}, compress:{f.compress}, {f.data_size} Bytes")
     # 获取响应：最大值跳过，其余从指定位置开始传输
-    shift = int.from_bytes(sock.recv(10), byteorder='big')
+    shift = int.from_bytes(sock.recv(8), byteorder='big')
     if (shift != 0xffffffffffffffff):
-        # 发送压缩标志和剩余数据长度
-        sock.send(int(f.compress).to_bytes(1, byteorder='big'))
-        sock.send((f.data_size-shift).to_bytes(10, byteorder='big'))
+        print(f"sending from {shift if shift!=0 else 'start'}")
+        # 发送剩余数据长度
+        sock.send((f.data_size-shift).to_bytes(8, byteorder='big'))
         while shift + 1024 < len(f.data):
             sock.send(f.data[shift:shift+1024])
             shift += 1024
         sock.send(f.data[shift:])
+        print("file sent")
     else:
         print(f"remote file exists, ignored")
 
--- src/server/server.py
+++ src/server/server.py
@@ -16,85 +16,91 @@ def main():
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((addr, port))
     sock.listen(1)
-    print(f"server listening at {addr}:{port}, files will be saved to {path}")
+    print(f"listening at {addr}:{port}")
 
     # 接收数据
     try:
         while True:
             conn, remote = sock.accept()
-            print(f"accepting files from {remote}")
+            print(f"{remote} accepted")
             while recv_file(conn, path):
                 pass
-            print(f"connection from {remote} fininshed")
+            print(f"{remote} fininshed")
             conn.close()
     except ConnectionResetError:
         print("remote closed")
     except KeyboardInterrupt:
-        print('manual exiting')
+        print('manual exit')
     sock.close()
 
 
-def recv_file(sock, path):
-    name_size = sock.recv(4)
+def aread(path):
+    data = bytearray()
+    with open(path, 'rb') as f:
+        buff = f.read(65535)
+        while buff:
+            data.append(buff)
+            buff = f.read(65535)
+    return bytes(data)
+
+
+def recv_file(conn, path):
+    name_size = conn.recv(4)
     if name_size:
         # 接收文件名长度
         name_size = int.from_bytes(name_size, byteorder='big')
         if(name_size == 0):
             return False
         # 接受文件名和文件摘要
-        name = sock.recv(name_size).decode('utf-8')
+        name = conn.recv(name_size).decode('utf-8')
         if "\\" in name:
             name = os.sep.join(name.split("\\"))
         elif "/" in name:
             name = os.sep.join(name.split("/"))
-        md5 = sock.recv(16)
-        print(f"file: {name},md5: {md5.hex()}")
+        md5 = conn.recv(16)
+        # 接受压缩标志
+        compress = int.from_bytes(conn.recv(1), byteorder='big') == 1
+        print(f"file: {name},md5: {md5.hex()}, compress:{compress}")
         # 中间文件和最终文件
         save = Path(path).joinpath(name)
         temp = Path(path).joinpath(name+'.download')
         # 断点续传、跳过已有和更新
         mode = 'wb'
+        shift = 0
         if temp.exists():  # 断点续传
-            print("incomplete download found, continuing preivous download")
-            sock.send(temp.stat().st_size.to_bytes(10, byteorder='big'))
+            print("continue previous download")
+            shift = temp.stat().st_size
             mode = 'ab'
         elif save.exists():
-            localmd5 = hashlib.md5()
-            with open(save, 'rb') as local:
-                buf = local.read(1024)
-                while buf:
-                    buf = local.read(1024)
-                    localmd5.update(buf)
-            if md5 == hashlib.md5(save.open('rb').read()).digest():  # 跳过已有
-                sock.send(int(0xffffffffffffffff).to_bytes(
-                    10, byteorder='big'))
+            if md5 == hashlib.md5(aread(save)).digest():  # 已有
+                shift = 0xffffffffffffffff
                 print("identical file exists, remote ignored")
-                return True
-            else:  # 更新文件
-                print("different file found in local, updating from remote")
-                sock.send(int(0).to_bytes(10, byteorder='big'))
-        else:
-            sock.send(int(0).to_bytes(10, byteorder='big'))
+            else:  # 更新
+                print("different exists on local, updating from remote")
+        # 反馈偏移
+        conn.send(shift.to_bytes(8, byteorder='big'))
+        # 跳过已有
+        if shift == 0xffffffffffffffff:
+            return
+        # 创建路径文件夹、写入临时文件
         Path(temp.parent).mkdir(parents=True, exist_ok=True)
-        # 写入临时文件
         with temp.open(mode) as out:
-            # 接受压缩标志和数据长度
-            compress = bool(int.from_bytes(sock.recv(1), byteorder='big'))
-            data_size = int.from_bytes(sock.recv(10), byteorder='big')
-            print(f"downloading {data_size} Bytes, compress={compress}")
+            # 接收数据长度
+            data_size = int.from_bytes(conn.recv(8), byteorder='big')
+            print(f"downloading {data_size} Bytes")
             # 创建、覆盖或追加文件
             while data_size > 1024:
-                out.write(sock.recv(1024))
+                out.write(conn.recv(1024))
                 data_size -= 1024
-            out.write(sock.recv(data_size))
+            out.write(conn.recv(data_size))
         # 解压缩并保存文件
-        data = temp.open('rb').read()
+        data = aread(temp)
         if compress:
-            data = zlib.decompressobj().decompress(data)
+            data = zlib.decompress(data)
         with open(save, 'wb') as out:
             out.write(data)
         os.remove(temp)
-        print('file saved')
+        print('file received')
     return True
 
 
